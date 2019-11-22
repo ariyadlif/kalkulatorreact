43a6b100b6389b93d9c9248786d5e8e5930b1dc4
User achmadriyadlifachmi <ar.arf.rf@gmail.com>
2019-11-22 19:57:05 +0700

    react kalkulator hari ke4

---
+++ src/content/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import '../style/style.css';
+
+import Tombol from './event/event';
+
+class App extends Component {
+  state = {
+    result: ''
+  };
+  buttonPressed = buttonName => {
+    if (buttonName === "=") {
+      this.calculate();
+    } else if (buttonName === "clear") {
+      this.reset();
+    } else if (buttonName === "CE") {
+      this.backspace()
+    } else
+      this.setState({
+        result: this.state.result + buttonName
+      });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      result: ''
+    });
+  };
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result)
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div className="wraperform">
+        <Tombol buttonPressed={this.buttonPressed} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/content/event/event.jsx
@@ -0,0 +1,49 @@
+import '../../style/style.css'
+
+import React, { Component } from 'react';
+
+class Tombol extends Component {
+
+    buttonPressed = e => {
+        this.props.buttonPressed(e.target.name);
+    };
+    
+    render() {
+        return (
+           
+            <div className="wraperform">
+                <from onSubmit={this.onSubmitHandler}>
+                    
+                    
+                    <div className="tombol">
+                      
+                    <br/>
+                    <br/>
+                    <br/>
+                    <button name="7" class="button_angka" value="7" onClick={this.buttonPressed}>1</button>
+                    <button name="8" value="8" class="button_angka" onClick={this.buttonPressed}>2</button>
+                    <button name="9" class="button_angka" onClick={this.buttonPressed} >3</button>
+                    <button name="/" class="button_simbol" onClick={this.buttonPressed}>/</button>
+                    <button name="4" class="button_angka" onClick={this.buttonPressed}>4</button>
+                    <button name="5" class="button_angka" onClick={this.buttonPressed}>5</button>
+                    <button name="6" class="button_angka" onClick={this.buttonPressed}>6</button>
+                    <button name="*" class="button_simbol" onClick={this.buttonPressed}>x</button>
+                    <button name="1" class="button_angka" onClick={this.buttonPressed}>7</button>
+                    <button name="2" class="button_angka" onClick={this.buttonPressed}>8</button>
+                    <button name="3" class="button_angka" onClick={this.buttonPressed}>9</button>
+                    <button name="-" class="button_simbol" onClick={this.buttonPressed}>-</button>
+                    <button name="0" class="zero" onClick={this.buttonPressed}>0</button>
+                    <button name="=" class="button_simbol" onClick={this.buttonPressed}>=</button>
+                    <button name="+" class="button_simbol" onClick={this.buttonPressed}>+</button>
+                    <button name="=" class="button_simbol2" onClick={this.buttonPressed}>hasil</button>
+
+                    </div>
+                    
+                </from>
+            </div>
+                
+        );
+    }
+}
+
+export default Tombol;
